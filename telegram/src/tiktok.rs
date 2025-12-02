fbc3a40309769f386ac6f64bc9bfe1886f025d48
Ramzy Zoldik <rabouzia@student.42.fr>
2025-12-02 19:21:44 +0100

    minor changes

--- telegram/src/tiktok.rs
+++ telegram/src/tiktok.rs
@@ -1,9 +1,66 @@
-// struct Tiktok {
-// 	video: bool,
-// 	img: bool,
-// // }
+use anyhow::{Context, Result, bail};
+#[derive(Debug, Default)]
+pub struct Tiktok {
+    video: bool,
+    img: bool,
+    url: String,
+    id: String,
+}
+
+impl Tiktok {
+    pub fn new(mut self, context: &'static str, _url: String, _id: String) -> Tiktok {
+        match context {
+            "video" => self.video = true,
+            "img" => self.img = true,
+            _ => {}
+        }
+        self.url = _url;
+        self.id = _id;
+        return self;
+    }
+}
 // https://d.rapidcdn.app/v2?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL3Y0NS1ldS50aWt0b2tjZG4uY29tLzVlYmU1ZDliYzg1NDc0MTE5YTNkZWVhMWI5MGE1YzNjLzY5MGU1YjJjL3ZpZGVvL3Rvcy9ubzFhL3Rvcy1ubzFhLXZlLTAwNjhjMDAxLW5vL280UFNEWXRIUXZiV2U0ZkFNQTZnR1FOZWpsendNTEFpSThMUlBJLz9hPTExODAmYnRpPWJHUnVaSHh2TVhJeGNtNTNabTFjWUY5ZWJXRnphSEZtT2clM0QlM0QmY2g9MCZjcj0wJmRyPTAmZXI9MCZjZD0wJTdDMCU3QzAlN0MwJmN2PTEmYnI9NTUyJmJ0PTI3NiZjcz0wJmRzPTYmZnQ9WHNGYjhxNGZtYmRQRDEyWndxV3Mzd1UxeFRReGFlRn5PNSZtaW1lX3R5cGU9dmlkZW9fbXA0JnFzPTAmcmM9TkRVNE5UeHBPRGM1TlRnNE9HVTdhRUJwTXpaeWRXNDVjbWM2TnpNemJ6Y3pOVUJmWW1Fd01TNDJYelF4TkdBd00ySXRZU051WTJjMU1tUnpiVE5oTFMxa01URnpjdyUzRCUzRCZ2dnBsPTEmbD0yMDI1MTEwNzA0NDc0NUVFOTU1NkQ2NTM5MDQ1OEZFMkFCJmJ0YWc9ZTAwMDhkMDAwIiwiZmlsZW5hbWUiOiJzbmFwdGlrXzc1Njk1NjMxNTM2OTMwMTk0MTVfdjIubXA0IiwiaGVhZGVycyI6eyJ1c2VyLWFnZW50IjoiVGVsZWdyYW1Cb3QgKGxpa2UgVHdpdHRlckJvdCkifSwiaWF0IjoxNzYyNDYyMDY1fQ.Z_-ye0Xr7tnw3Di2wQL7QKshK7sx8FTGBd2VO-Q2rbE&dl=1
 
 // https://vm.tiktok.com/ZNdwh5QQP/
 
 // https://tikcdn.io/ssstik/ZNdwh5QQP
+
+pub fn tiktok_url_parser(handle: String) -> Result<String> {
+    let extract: Vec<&str> = handle.splitn(6, "/").collect();
+    if extract.len() <= 1 {
+        bail!("profile_pic_url_hd not found in JSON.");
+    }
+    match extract[2] {
+        "tiktok.com" | "vm.tiktok.com" | "vt.tiktok.com" => {
+            ();
+        }
+        _ => {}
+    }
+    let mut mid = extract[5];
+    if mid.contains("?") {
+        let last: Vec<&str>;
+        last = mid.split("?").collect();
+        println!("new URL: {}", last[0]);
+        mid = last[0];
+    }
+
+    return Ok(mid.to_string());
+}
+
+pub async fn tiktok(handle: String) -> Result<Vec<Tiktok>> {
+    let vt: Vec<Tiktok> = Vec::new();
+    let last: String = tiktok_url_parser(handle)?;
+
+    let scrapper_key = env::var("TK_SCRAPPER").context("TK_SCRAPPER not found in .env")?;
+
+
+    let _body = reqwest::get(&format!("{}{}", scrapper_key, last))
+        .await
+        .context("REASON")?
+        .text()
+        .await
+        .context("REASON")?;
+
+    // let  mut easy = Easy::new();
+    Ok(vt)
+}
--- telegram/src/x.rs
+++ telegram/src/x.rs
@@ -5,7 +5,9 @@
 // 	id: String,
 // }
 
-use rustc_serialize::json::Json;
+// use rustc_serialize::json::Json;
+use anyhow::{Context, Result, anyhow, bail};
+use serde_json::Value;
 
 #[derive(Debug, Default)]
 pub struct X {
@@ -30,20 +32,16 @@ impl X {
     }
 }
 
-pub fn x_url_parser(handle: String) -> Result<String, bool> {
+pub fn x_url_parser(handle: String) -> Result<String> {
     let extract: Vec<&str> = handle.splitn(6, "/").collect();
     if extract.len() <= 1 {
-        return Err(false);
+        bail!("Invalid domain expect more segment in the url")
     }
     // println!("Twitter URL: {}", extract[5]);
     // println!("Twitter last: {}", extract[2]);
     match extract[2] {
-        "x.com" | "twitter.com" => {
-            ();
-        }
-        _ => {
-            return Err(false);
-        }
+        "x.com" | "twitter.com" => {}
+        _ => bail!("Invalid domain"),
     }
     let mut mid = extract[5];
     if mid.contains("?") {
@@ -53,68 +51,68 @@ pub fn x_url_parser(handle: String) -> Result<String, bool> {
         mid = last[0];
     }
 
-    return Ok(mid.to_string());
+    Ok(mid.to_string())
 }
 
-// fn get_id() -> String
-// {
-
-// }
-
-// async fn twitter(handle: String, mut vec: Vec<String>) -> X
-pub async fn twitter(handle: String) -> Result<Vec<X>, bool> {
+pub async fn twitter(handle: String) -> Result<Vec<X>> {
     let mut vx: Vec<X> = Vec::new();
     let last: String = x_url_parser(handle)?;
     // let  mut easy = Easy::new();
-    let scrapper_key = env::var("X_SCRAPPER").expect("X_SCRAPPER not found in .env");
+    let scrapper_key = env::var("X_SCRAPPER").context("X_SCRAPPER not found in .env")?;
     let body = reqwest::get(&format!("{}{}", scrapper_key, last))
         .await
-        .expect("REASON")
+        .context("REASON")?
         .text()
         .await
-        .expect("REASON");
-    // println!("Body: {body:?}");
-    let json = Json::from_str(&body).expect("REASON");
-    // println!("{}",json);
-    let media = json["data"]["media"].as_array().expect("REASON");
-    for item in media {
-        let media_type = item["type"].as_string().unwrap_or("unknown");
-        let caca = item["url"].as_string().unwrap_or_default();
+        .context("REASON")?;
 
-        // Extract the file name from the URL (without extension)
-        let jsom = caca
+    // println!("Body: {body:?}");
+    let json: Value = serde_json::from_str(&body)?;
+
+    // println!("{}",json);
+    let media = json["data"]["media"]
+        .as_array()
+        .ok_or_else(|| anyhow!("Missing data->media"))?;
+
+    for item in media {
+        let media_type = item["type"]
+            .as_str()
+            .ok_or_else(|| anyhow!("Missing 'type' in media item"))?;
+        // let caca = item["url"].as_str().unwrap_or_default();
+        let url = item["url"]
+            .as_str()
+            .ok_or_else(|| anyhow!("Missing 'type' in media item"))?;
+
+        let jsom = url
             .rsplit('/')
             .next()
-            .and_then(|filename| filename.split_once('.').map(|(name_no_ext, _)| name_no_ext))
-            .unwrap_or("unknown")
+            .and_then(|filename| filename.split_once('.').map(|(name, _)| name))
+            .ok_or_else(|| anyhow!("Invalid URL format: {}", url))?
             .to_string();
 
-        // Print debugging info
         println!("media type: {}", media_type);
         println!("new URL: {}", jsom);
 
         match media_type {
             "photo" => {
-                let url = item["url"].as_string().unwrap_or_default();
                 println!("Image URL: {}", url);
                 let ix = X::default().new("img", url.to_string(), jsom.clone());
                 vx.push(ix);
             }
 
             "video" => {
-                let video_url = item["videoUrl"].as_string().unwrap_or_default();
+                let video_url = item["videoUrl"].as_str().unwrap_or_default();
                 println!("Video URL: {}", video_url);
-                let ok =X::default().new("video", video_url.to_string(), jsom.clone());
+                let ok = X::default().new("video", video_url.to_string(), jsom.clone());
                 vx.push(ok);
             }
 
             _ => {
                 println!("Unknown media type: {}", media_type);
             }
-
         }
     }
-    return Ok(vx);
+    Ok(vx)
 }
 
 /*
