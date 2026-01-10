e678a2f0821fb374ec7f512423fee56cb4179140
ibaby <ibaby@kfs.myguest.virtualbox.org>
2026-01-10 22:56:12 +0100

    Added a prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,12 @@ pub mod telegram;
 
 #[cfg(feature = "twitter")]
 mod twitter;
+
+pub mod prelude {
+    pub use crate::core::error::{Error, BotResult};
+	pub use crate::core::traits::{MediaScraper, MediaSender};
+	pub use crate::core::types::{MediaKind, MediaMetadata};
+
+	pub use crate::telegram::TelegramBot;
+	pub use crate::twitter::TwitterScraper;
+}
